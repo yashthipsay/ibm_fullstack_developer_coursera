87e9540f4cdd78bc13a6cae2e8c225ed09596857
Yash Thipsay <yashthpsay@gmail.com>
2024-10-14 10:18:15 +0530

    frontend developer course

--- Developing Applications with SQL, Databases, and Django/ormtemplate/standalone/models.py
+++
@@ -1,5 +0,0 @@
-from django.db import models
-
-# Test model
-class Test(models.Model):
-    name = models.CharField(max_length=30)
--- Python Project for AI & Application Development/mymath/geometry.py
+++
@@ -1,7 +0,0 @@
-def area_of_rectangle(length,breadth):
-    return length * breadth
-
-
-def area_of_circle(radius):
-    return (3.141592654) * (radius)**2
-
